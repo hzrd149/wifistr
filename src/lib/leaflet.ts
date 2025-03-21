c208c9adaf3ebce33f574600ad4856dccc3dba0f
hzrd149 <github@hzrd149.com>
2025-03-21 07:42:34 +0000

    add pwa icons

--- src/lib/leaflet.ts
+++ src/lib/leaflet.ts
@@ -2,14 +2,10 @@ import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import "leaflet.locatecontrol/dist/L.Control.Locate.css";
 
-import marker2x from "leaflet/dist/images/marker-icon-2x.png?url";
-import marker from "leaflet/dist/images/marker-icon.png?url";
-import shadow from "leaflet/dist/images/marker-shadow.png?url";
-
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: marker2x,
-  iconUrl: marker,
-  shadowUrl: shadow,
+  iconRetinaUrl: "/markers/marker-icon-2x.png",
+  iconUrl: "/markers/marker-icon.png",
+  shadowUrl: "/markers/marker-shadow.png",
 });
 
 export default L;
--- vite.config.ts
+++ vite.config.ts
@@ -25,6 +25,23 @@ export default defineConfig({
       manifest: {
         name: "Wifistr",
         theme_color: "#0000ff",
+        icons: [
+          { src: "/favicon.ico", type: "image/x-icon", sizes: "16x16 32x32" },
+          { src: "/icon-192.png", type: "image/png", sizes: "192x192" },
+          { src: "/icon-512.png", type: "image/png", sizes: "512x512" },
+          {
+            src: "/icon-192-maskable.png",
+            type: "image/png",
+            sizes: "192x192",
+            purpose: "maskable",
+          },
+          {
+            src: "/icon-512-maskable.png",
+            type: "image/png",
+            sizes: "512x512",
+            purpose: "maskable",
+          },
+        ],
       },
     }),
   ],
